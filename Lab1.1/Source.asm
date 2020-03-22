06327c09faad34e1bb7d13c56e12aa8e8791f205
Alex <alskoupa@gmail.com>
2020-03-22 12:56:25 +0200

    skeleton + declaration of variables and symbolic constants

---
+++ Lab1.1/Source.asm
@@ -0,0 +1,15 @@
+title Laboratory 1 : Evaluating Simple Arithmetic Expressions
+INCLUDE Irvine32.inc
+	p EQU 8d
+.data
+	w SDWORD ?
+	x SDWORD 16d
+	y SDWORD 154D
+	z SDWORD -17d
+
+.code
+main PROC
+
+	exit
+main ENDP
+END main
